537ad947dcc7d8e458ea8fcd9986c5a90c31d346
Dario Velez Roman <dvelezroman@gmail.com>
2024-08-21 07:27:45 -0500

    Enhance auth guard by integrating JWT validation
    Added JSON Web Token (JWT) validation to the auth guard to ensure that only admin users can access protected routes. The implementation includes decoding and verifying the token, logging token details, and redirecting non-admin users to the home page. Updated `package.json` to include `jsonwebtoken` and its type definitions.

--- src/app/components/auth/auth.guard.ts
+++ src/app/components/auth/auth.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as jwt from 'jsonwebtoken';
 import { CanActivate, Router } from '@angular/router';
 import {UserService} from "../../services/user.service";
 
@@ -12,10 +13,23 @@ export class AuthGuard implements CanActivate {
 
   canActivate(): boolean {
     const token = typeof window !== 'undefined' ? localStorage.getItem('authToken') : '';
+
     if (token) {
-      return true;
+      try {
+        const decodedToken: any = jwt.decode(token);
+        console.log(decodedToken);
+        if (decodedToken && decodedToken.isAdmin) {
+          return true; // Token exists and user is admin
+        } else {
+          this.router.navigate(['/home']); // Redirect to home if not admin
+          return false;
+        }
+      } catch (error) {
+        this.router.navigate(['/home']); // Redirect if token decoding fails
+        return false;
+      }
     } else {
-      this.router.navigate(['/home']);
+      this.router.navigate(['/home']); // Redirect to home if no token
       return false;
     }
   }
