437f70e5f72f19fe555ee6b47b4ab817c578ce9f
Dario Velez Roman <dvelezroman@gmail.com>
2024-08-22 12:22:30 -0500

    Update environment API URL to production
    Reordered the API URLs to make the production URL active. Kept the development URL as a comment for future use. This change ensures the application uses the production API by default.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3300/v0', // Development API URL 'http://mundialito-api.porto-rockcity-app.com/v0',
+  apiUrl: 'http://mundialito-api.porto-rockcity-app.com/v0',
+  // apiUrl: 'http://localhost:3300/v0',
 };
