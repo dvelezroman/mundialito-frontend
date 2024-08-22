c64dc94f46bd30b37419ad99e0c46f55820b4472
Dario Velez Roman <dvelezroman@gmail.com>
2024-08-22 12:47:40 -0500

    Remove environment configuration files
    Deleted both production and development environment configuration files to streamline the project setup. These files contained sensitive information and are now managed through a more secure method.

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://mundialito-api.porto-rockcity-app.com/v0', // Production API URL
-  aws_region: 'us-east-1',
-  s3BucketName: 'mundialito-portoviejo'
-};
