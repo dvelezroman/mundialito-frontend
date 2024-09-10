d4738bbcf840b77db8e68163342601277e48daee
greverom <greverom4@gmail.com>
2024-09-10 11:38:49 -0500

    estilos reportes movil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,17 +7,18 @@ import {PersonComponent} from "./components/person/person.component";
 import {TeamComponent} from "./components/team/team.component";
 import {PlayerCardsComponent} from "./components/player-cards/player-cards.component";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
-import {ReportesComponent} from "./components/reportes/reportes.component";
+import { ReportesComponent } from './components/reportes/reportes.component';
+
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'people', component: PersonComponent, canActivate: [AuthGuard] },
-  { path: 'teams', component: TeamComponent },
+  { path: 'teams', component: TeamComponent, canActivate: [AuthGuard] },
   { path: 'player-cards/:teamId', component: PlayerCardsComponent, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'reportes', component: ReportesComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'reportes', component: ReportesComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/dashboard' }, // Handle unknown routes
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
