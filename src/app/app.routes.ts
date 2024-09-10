cd5d78cac97534336556a14aba29c50b35aa28be
Dario Velez Roman <dvelezroman@gmail.com>
2024-09-10 08:42:04 -0500

    Rename and relocate ReportesComponent
    Moved ReportesComponent from pages to components for better organization. Updated import paths and route configuration accordingly.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import {PersonComponent} from "./components/person/person.component";
 import {TeamComponent} from "./components/team/team.component";
 import {PlayerCardsComponent} from "./components/player-cards/player-cards.component";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
-import { ReportesComponent } from './pages/reportes/reportes.component';
+import {ReportesComponent} from "./components/reportes/reportes.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
